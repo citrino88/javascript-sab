038603acf1907b26609326f250761df8835d0585
Almeida Dayane <almeida.citrino@gmail.com>
2024-08-10 09:32:17 -0300

    switch case inicial e exemplo de while

---
+++ aulas/aula04/script.js
@@ -0,0 +1,41 @@
+'use strict';
+
+console.log("ola mundo");
+alert("ola mundo!");
+
+
+
+/* switch (mesNascimento) {
+    case 'janeiro':
+        alert('voce é capricorniano');  
+        break;
+    case 'fevereiro':
+        alert('voce é pisciano');  
+        break;
+    case 'outubro':
+        alert('voce é libriano');  
+        break;            
+
+    default:
+        alert('voce escreveu o mes errado');
+        break;
+} */
+
+var numero = 2;
+
+/* console.log(numero*1);
+console.log(numero*2);
+console.log(numero*3);
+console.log(numero*4);
+console.log(numero*5);
+console.log(numero*6);
+console.log(numero*7);
+console.log(numero*8);
+console.log(numero*9);
+console.log(numero*10); */
+
+var contador = 1;
+while(contador <= 10){
+    console.log(numero*contador);
+    contador++;//contator mais 1
+}
