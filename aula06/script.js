86e54e638303020f18d8440d0c2a6c4735a388ce
Almeida Dayane <almeida.citrino@gmail.com>
2024-08-24 09:21:49 -0300

    Dom JS

---
+++ aula06/script.js
@@ -0,0 +1,15 @@
+console.log("ola mundo!");
+
+//console.log(document.getElementById('tituloPrincipal'));
+
+// Mas vamos guardar dentro da variavel, const pois nao vamos mudar o valor dela.
+
+const tituloPrincipal = document.getElementById('tituloPrincipal');
+
+console.log(tituloPrincipal);
+
+tituloPrincipal.textContent = "Teste"
+
+var nome= prompt('Digite seu nome:: ');
+
+tituloPrincipal.textContent = `Seja bem vinde ${nome}`;
